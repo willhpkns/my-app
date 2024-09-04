61abc35f04fe4385fba5ac9ccd1f562911e3f631
Will <williamraymondhopkins@gmail.com>
2024-09-04 17:33:00 +0800

    added mongoDB

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import { LeaderboardEntry } from '@/components/types';
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI as string
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
